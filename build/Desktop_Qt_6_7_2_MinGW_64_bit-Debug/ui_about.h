e0b4a7ffbad70b9a333936b9884b596f043211a6
jiajia123456688 <djwdjw112233@qq.com>
2024-09-20 19:43:01 +0800

    作业2

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_about.h
@@ -0,0 +1,63 @@
+/********************************************************************************
+** Form generated from reading UI file 'about.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ABOUT_H
+#define UI_ABOUT_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_about
+{
+public:
+    QTextEdit *textEdit;
+
+    void setupUi(QWidget *about)
+    {
+        if (about->objectName().isEmpty())
+            about->setObjectName("about");
+        about->resize(349, 233);
+        textEdit = new QTextEdit(about);
+        textEdit->setObjectName("textEdit");
+        textEdit->setGeometry(QRect(100, 70, 161, 91));
+
+        retranslateUi(about);
+
+        QMetaObject::connectSlotsByName(about);
+    } // setupUi
+
+    void retranslateUi(QWidget *about)
+    {
+        about->setWindowTitle(QCoreApplication::translate("about", "Form", nullptr));
+        textEdit->setHtml(QCoreApplication::translate("about", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Microsoft YaHei UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\345\247\223\345\220\215"
+                        "\357\274\232\351\202\223\345\230\211\347\273\264</p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\345\255\246\345\217\267\357\274\2322022414040208</p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class about: public Ui_about {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ABOUT_H
